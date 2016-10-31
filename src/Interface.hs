723401d893f17486af5519ba080e8f2165879235
Markus Lindberg <markus.lindberg89@gmail.com>
2016-11-01 00:10:28 +0100

    refactor, getFileParser

--- src/Interface.hs
+++ src/Interface.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Interface where
 
 import           Window (Window(..), initWindow)
 import           Parser
 
 import qualified Data.Text.Lazy as T
+import           Data.List                   (nub)
+import           Data.Maybe                  (catMaybes)
 import           Control.Monad.Trans.Except
 import           Control.Monad.Identity
 import           Control.Monad.Reader
@@ -14,25 +17,51 @@ data InterfaceOptions =
                      sepChar  :: Maybe String,
                      groups   :: Maybe Int,
                      position :: Maybe Int,
-                     values   :: Maybe Int,
+                     iValues   :: Maybe Int,
                      wSize    :: Integer,
                      wStep    :: Integer,
-                     header   :: Bool}
+                     iHeader   :: Bool}
   deriving (Show, Eq)
 
 newtype FileParserT m a = FileParserT { parse :: [T.Text] -> [ExceptT String m a] }
 type FileParser = FileParserT Identity
+type Interface m a = ReaderT InterfaceOptions (ExceptT String m) a
 
-
-getStartWindow :: (Monad m) => ReaderT InterfaceOptions m (Window Integer b)
+getStartWindow :: (Monad m) => Interface m (Window Integer b)
 getStartWindow = do
   size <- asks wSize
   return $ initWindow size
 
-getFileParser :: (Monad m) => ReaderT InterfaceOptions (ExceptT String m) (FileParser a)
-getFileParser = undefined
+getFileParser :: (Monad m) => Interface m (FileParser (ParseResult Integer Double))
+getFileParser = do
+  separator <- asks sepChar
+  groupCol  <- asks groups
+  posCol    <- asks position
+  valCol    <- asks iValues
+  hasHead   <- asks iHeader
+  guardColumns valCol groupCol posCol
+  return $ FileParserT (map (lineParser' separator groupCol posCol valCol) . (skipHead hasHead) . zip [1..])
   
-
+  where
+    skipHead h = if h then drop 1 else id
+    lineParser' s g p v =
+          let parser = parseLine (maybe (T.pack "\t") T.pack s) g p (maybe 1 id v)
+          in (\(i,x) -> catchE (parser x) (\e -> throwE $ "Parse error at line " ++ show i ++ ". " ++ e))
+          
+     
+    guardColumns Nothing Nothing Nothing = pure ()
+    guardColumns Nothing _ _             = lift $ throwE
+                                                  ("Need to know what column to use"  ++
+                                                   " as values when columns for " ++
+                                                   "position or groups are specified")
+    guardColumns a b c
+      | nub xs == xs = pure ()
+      | otherwise    = lift (throwE
+                            "Cannot have the same column specified multiple times")
+      where
+        xs = catMaybes [a,b,c]
+      
+  
 options :: Parser InterfaceOptions
 options = InterfaceOptions <$>
    (optional $ strOption
--- src/Parser.hs
+++ src/Parser.hs
@@ -4,39 +4,36 @@ module Parser (
 
 import           Control.Applicative         ((<*>), (<$>))
 import           Control.Monad.Trans         (lift)
-import           Control.Monad.Trans.Except  (ExceptT, throwE, mapExceptT)
+import           Control.Monad.Trans.Except  (ExceptT, throwE, mapExceptT, catchE)
 import qualified Data.Text.Lazy as T
 import           Data.Text.Lazy.Read         (Reader, decimal, double)
 
-
+catchE
 data ParseResult a b = ParseResult { group :: Maybe T.Text,
                                      posit :: Maybe a,
                                      value :: b}
   deriving (Show, Eq)
 
 parseLine :: (Num b, Monad m) =>
-              Char ->
-              Maybe Int ->
-              Maybe Int ->
-              Int ->
-              T.Text ->
+              T.Text ->     -- delimiter
+              Maybe Int ->  -- optional column position for group
+              Maybe Int ->  -- optional column position for position
+              Int ->        -- column position for values
+              T.Text ->     -- line to parse
               ExceptT String m (ParseResult Integer Double)
 parseLine delim grCol posCol valCol x = ParseResult <$> getGroup <*> getPos <*> getVal
   where
-    parts    = zip [1..] $ T.splitOn (T.pack [delim]) x -- split up the line and make it searchable
     getGroup = maybeLookupColumn grCol "group" pure
     getPos   = maybeLookupColumn posCol "position" (read' decimal) 
---    getPos   = maybe (pure Nothing)
---                     (\x -> Just <$> (lookupColumn (errMsg "position") x parts >>= read' decimal))
---                     posCol
-
     getVal   = lookupColumn (errMsg "value") valCol parts >>= read' double
-    errMsg e = "Couldn't parse column for " ++ e ++ ". Is the column argument correct?"
-
     maybeLookupColumn col e f = maybe (pure Nothing)
                                       (\x -> Just <$> (lookupColumn (errMsg e) x parts >>= f))
                                        col
+    parts    = zip [1..] $ T.splitOn delim x -- split up the line and make it searchable
+    errMsg e = "Couldn't parse column for " ++ e ++ ". Is the column argument correct?"
 
+
+-- Helper functions
 lookupColumn :: (Ord a, Monad m) => String -> a -> [(a, b)] -> ExceptT String m b
 lookupColumn e i xs = case lookup i xs of
   Nothing -> throwE e
